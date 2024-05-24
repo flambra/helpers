473aea7eee0d7049d9a4b66187e87545f3d895b2
hjunior29 <helderjuniorsilvalima@gmail.com>
2024-05-24 02:40:59 -0300

    🎉 jwt generate

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.22
 require (
 	github.com/gofiber/fiber/v2 v2.52.4
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	golang.org/x/crypto v0.22.0
+	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.9
 )
 
@@ -27,5 +29,4 @@ require (
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gorm.io/driver/postgres v1.5.7 // indirect
 )
---
+++ hAuth/jwt.go
@@ -0,0 +1,48 @@
+package hAuth
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type User struct {
+	ID    int
+	Email string
+}
+
+// GenerateJWT generates a JSON Web Token (JWT) for the given user.
+// It takes a User object as input and returns the generated JWT as a string.
+// If an error occurs during token generation, it returns an empty string and the error.
+//
+// Example usage:
+//   token, err := hAuth.GenerateJWT(user)
+//   if err != nil {
+//     return hResp.InternalServerErrorResponse(c, err.Error())
+//   }
+//
+//   return hResp.SuccessResponse(c, fiber.Map{"token": token})
+//
+// The function requires the following environment variables to be set:
+//   - TOKEN_SECRET_KEY: The secret key used for signing the token.
+//   - TOKEN_DURATION: The duration (in hours) for which the token is valid, e.g., "1", "0.5", etc.
+func GenerateJWT(user User) (string, error) {
+	secretKey := os.Getenv("TOKEN_SECRET_KEY")
+	durationStr := os.Getenv("TOKEN_DURATION")
+
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return "", err
+	}
+
+	claims := jwt.MapClaims{
+		"user_id": user.ID,
+		"email":   user.Email,
+		"exp":     time.Now().Add(duration).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secretKey))
+}
